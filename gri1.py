35792a1616fdb0467918d76cc6deb1448531bc64
zzz <2206321864@qq.com>
2019-06-22 14:11:58 +0800

    分布执行

--- gri1.py
+++ gri1.py
@@ -3,13 +3,24 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.Remote(
-    command_executor="http://172.168.50.115:7778/wd/hub",
+    command_executor="http://192.168.0.100:6655/wd/hub",
     desired_capabilities= {
         "browserName" : 'chrome',
         "video": 'true',
-        'platform':'MAC'
+         'platform': 'WINDOWS',
+         'platformName': 'WINDOWS'
     }
 
 )
-driver.get('http://www.baidu.com')
-time.sleep(3)
+
+# time.sleep(3)
+# from selenium import webdriver
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_argument('--headless')
+# chrome_options.add_argument('--disable-gpu')
+# driver = webdriver.Chrome(chrome_options=chrome_options)
+# driver.get('http://www.baidu.com')
+# driver.save_screenshot('ceshi1.png')
+
+# driver = webdriver.Chrome()
+# driver.get('http://www.baidu.com')
